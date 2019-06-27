07c4e5f8f23f97cb51c9cb7355da09a112a2ab27
pengheng <254124185@qq.com>
2019-06-27 11:42:32 +0800

    修改web配置 添加cors跨域配置

--- sunnsoft-admin/src/main/java/com/pengheng/config/WebMvcConfig.java
+++ sunnsoft-admin/src/main/java/com/pengheng/config/WebMvcConfig.java
@@ -1,29 +1,19 @@
 package com.pengheng.config;
 
-import com.pengheng.core.interceptor.CommonIntercepter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Autowired
-    private CommonIntercepter commonIntercepter;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         String homePage = "/index.html";
         registry.addViewController("/").setViewName("redirect:" + homePage);
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(commonIntercepter).addPathPatterns("/**").excludePathPatterns("/login", "/register");
     }
 
     @Override
