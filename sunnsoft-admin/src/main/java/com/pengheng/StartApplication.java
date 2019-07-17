397af48589fb192bca8b813f882c0f079fb7481f
pengheng <254124185@qq.com>
2019-07-18 00:56:15 +0800

    添加过滤器 防止 xss攻击

--- sunnsoft-admin/src/main/java/com/pengheng/StartApplication.java
+++ sunnsoft-admin/src/main/java/com/pengheng/StartApplication.java
@@ -3,10 +3,12 @@ package com.pengheng;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.github.pagehelper.autoconfigure.PageHelperAutoConfiguration;
 
+@ServletComponentScan
 @EnableTransactionManagement
 //自动扫描所有的Mapper类
 @MapperScan(value="com.pengheng.mapper")
