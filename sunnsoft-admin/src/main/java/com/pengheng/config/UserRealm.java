801fefbafcfb1ae9ac39aad52ca9408859f8f3c3
pengheng <254124185@qq.com>
2019-07-18 00:35:54 +0800

    登录后记录用户权限，避免每次校验重新查询用户权限

--- sunnsoft-admin/src/main/java/com/pengheng/config/UserRealm.java
+++ sunnsoft-admin/src/main/java/com/pengheng/config/UserRealm.java
@@ -24,51 +24,55 @@ import com.pengheng.util.Toolkits;
 
 public class UserRealm extends AuthorizingRealm {
 
-	@Autowired
-	private IDynamicSqlService dynamicSqlService;
-	
-	/**
-	 * 执行认证逻辑
-	 */
-	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
-		String password = null;// 数据库返回密码
+    @Autowired
+    private IDynamicSqlService dynamicSqlService;
 
-		CriterionVo criterionVo = new CriterionVo();
-		criterionVo.addCondition("login_name", usernamePasswordToken.getUsername());
-		//查询用户信息
-		Map<Object, Object> userMap = dynamicSqlService.dynamicSelectUnique("sys_user", criterionVo);
-		if (MapUtils.isEmpty(userMap))// 如果没找到集合对象
-		{
-			throw new UnknownAccountException("账号或密码不正确");// 表示用户不存在
-		}
-		password = Toolkits.defaultString(userMap.get("password"));
+    /**
+     * 执行认证逻辑
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
+        String password = null;// 数据库返回密码
 
-		return new SimpleAuthenticationInfo(userMap, password, "");
-	}
+        CriterionVo criterionVo = new CriterionVo();
+        criterionVo.addCondition("login_name", usernamePasswordToken.getUsername());
+        //查询用户信息
+        Map<Object, Object> userMap = dynamicSqlService.dynamicSelectUnique("sys_user", criterionVo);
+        if (MapUtils.isEmpty(userMap))// 如果没找到集合对象
+        {
+            throw new UnknownAccountException("账号或密码不正确");// 表示用户不存在
+        } else {
+            //查询用户的所有权限
+            StringBuffer sb = new StringBuffer();
+            sb.append("select sys_menu.perms from sys_menu");
+            sb.append(" left join sys_role_menu on sys_menu.id = sys_role_menu.menu_id ");
+            sb.append(" left join sys_role on sys_role_menu.role_id = sys_role.id");
+            sb.append(" left join sys_user_role on sys_role.id = sys_user_role.role_id");
+            sb.append(" left join sys_user on sys_user_role.user_id = sys_user.id");
+            sb.append(" where sys_user.id = " + Toolkits.defaultString(userMap.get("id")) + " and sys_menu.perms <> ''");
+            List<Map<Object, Object>> permsList = dynamicSqlService.dynamicSelect("(" + sb + ") as tb");
+            userMap.put("permsList", permsList);
+        }
+        password = Toolkits.defaultString(userMap.get("password"));
 
-	/**
-	 * 执行授权逻辑
-	 */
-	@Override
-	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-		Subject subject = SecurityUtils.getSubject();
-		SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
-		Map<Object,Object> userMap = (Map<Object, Object>) subject.getPrincipal();
-		//查询用户的所有权限
-		StringBuffer sb = new StringBuffer();
-		sb.append("select sys_menu.perms from sys_menu"); 
-		sb.append(" left join sys_role_menu on sys_menu.id = sys_role_menu.menu_id ");
-		sb.append(" left join sys_role on sys_role_menu.role_id = sys_role.id"); 
-		sb.append(" left join sys_user_role on sys_role.id = sys_user_role.role_id"); 
-		sb.append(" left join sys_user on sys_user_role.user_id = sys_user.id"); 
-		sb.append(" where sys_user.id = "+Toolkits.defaultString(userMap.get("id"))+" and sys_menu.perms <> ''"); 
-		List<Map<Object, Object>> permsList = dynamicSqlService.dynamicSelect("("+sb+") as tb");
-		for (Map<Object, Object> map : permsList) {
-			authorizationInfo.addStringPermission(Toolkits.defaultString(map.get("perms")));
-		}
-		return authorizationInfo;
-	}
+        return new SimpleAuthenticationInfo(userMap, password, "");
+    }
+
+    /**
+     * 执行授权逻辑
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        Subject subject = SecurityUtils.getSubject();
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        Map<Object, Object> userMap = (Map<Object, Object>) subject.getPrincipal();
+
+        List<Map<Object, Object>> permsList = (List<Map<Object, Object>>) userMap.get("permsList");
+        for (Map<Object, Object> map : permsList) {
+            authorizationInfo.addStringPermission(Toolkits.defaultString(map.get("perms")));
+        }
+        return authorizationInfo;
+    }
 
 }
