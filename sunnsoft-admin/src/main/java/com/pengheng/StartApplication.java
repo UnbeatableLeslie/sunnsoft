989c476c4ec6199cd93ef8113338f02279f3f9e8
pengheng <254124185@qq.com>
2019-07-19 11:27:50 +0800

    通过aop 输出请求和返回参数

--- sunnsoft-admin/src/main/java/com/pengheng/StartApplication.java
+++ sunnsoft-admin/src/main/java/com/pengheng/StartApplication.java
@@ -4,7 +4,6 @@ import com.github.pagehelper.autoconfigure.PageHelperAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ServletComponentScan
 @SpringBootApplication(exclude = PageHelperAutoConfiguration.class)
---
+++ sunnsoft-admin/src/main/java/com/pengheng/config/LogAspect.java
@@ -0,0 +1,41 @@
+package com.pengheng.config;
+
+import com.pengheng.util.Toolkits;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private static Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    @Pointcut("execution(public * com.pengheng.controller..*.*(..))")
+    public void logPointCut() {
+    }
+
+    @Before("logPointCut()")
+    public void before(JoinPoint joinpoint){
+        Object [] args = joinpoint.getArgs();
+        MethodSignature signature = (MethodSignature) joinpoint.getSignature();
+        Method method = signature.getMethod();
+        logger.info("{}.{}:请求参数:{}",method.getDeclaringClass().getName(),method.getName(), Arrays.toString(joinpoint.getArgs()));
+    }
+
+    @AfterReturning(value = "logPointCut()",returning = "ret")
+    public void after(JoinPoint joinpoint,Object ret){
+        MethodSignature signature = (MethodSignature) joinpoint.getSignature();
+        Method method = signature.getMethod();
+        logger.info("{}.{}:返回参数:{}",method.getDeclaringClass().getName(),method.getName(), Toolkits.toJson(ret));
+    }
+}
