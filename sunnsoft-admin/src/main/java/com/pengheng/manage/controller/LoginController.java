1fa619dfa73a75475a5bcc5ae6f754610cca8f23
pengheng <254124185@qq.com>
2019-09-03 14:58:51 +0800

    抽出公共方法

--- sunnsoft-admin/src/main/java/com/pengheng/manage/controller/LoginController.java
+++ sunnsoft-admin/src/main/java/com/pengheng/manage/controller/LoginController.java
@@ -73,35 +73,6 @@ public class LoginController {
 
 	}
 
-	/**
-	 * 登出方法
-	 * @return
-	 */
-	@RequestMapping(value = "/logout", method = {RequestMethod.GET, RequestMethod.POST})
-	public ResultVo logout() {
-		Subject subject = SecurityUtils.getSubject();
-		subject.logout();
-		return new ResultVoSuccess("登出成功");
-	}
-
-	/**
-	 * shiro过滤未登录跳转提示
-	 * @return
-	 */
-	@RequestMapping(value = "/unlogin", method = {RequestMethod.GET, RequestMethod.POST})
-	public ResultVo unlogin() {
-		return new ResultVo("401","用户未登录");
-	}
-
-	/**
-	 * shiro过滤无权限跳转
-	 * @return
-	 */
-	@RequestMapping(value = "/unauth", method = {RequestMethod.GET, RequestMethod.POST})
-	public ResultVo unauth() {
-		return new ResultVoSuccess("401","用户无权限");
-	}
-
 	@GetMapping("/export")
 	public void export(HttpServletResponse response,HttpServletRequest request) throws Exception {
 		String excelName = "导出数据";
