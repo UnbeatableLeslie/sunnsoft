df5e5b925710e097d82121b788b6fabdd900f380
AlanViast <AlanViast@gmail.com>
2019-09-10 11:23:10 +0800

    修改响应对象

--- sunnsoft-framework/src/main/java/com/pengheng/model/ResultVo.java
+++ sunnsoft-framework/src/main/java/com/pengheng/model/ResultVo.java
@@ -3,23 +3,43 @@ package com.pengheng.model;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.github.pagehelper.PageInfo;
 import com.pengheng.util.Toolkits;
+import lombok.Data;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+/**
+ * @author pengheng
+ */
+@Data
 public class ResultVo implements Serializable {
 
     private static final long serialVersionUID = -7535228184843182772L;
+
     private boolean encrypt = false;
-    private String code = "";//返回的code
-    private String msg = ""; //返回的信息
-    private Object data = null;//返回的结果集合
-    private Object pageInfo = new HashMap<Object, Object>();//返回的分页集合
+
+    /**
+     * 返回的code
+     */
+    private String code;
+
+    /**
+     * 返回的信息
+     */
+    private String msg;
+
+    /**
+     * 返回的结果集合
+     */
+    private Object data;
+
+    /**
+     * 返回的分页集合
+     */
+    private Object pageInfo;
+
     private String timestamp = String.valueOf(System.currentTimeMillis());
     private String signature = "";
     private String createTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(Long.parseLong(this.timestamp)));
@@ -28,27 +48,26 @@ public class ResultVo implements Serializable {
     }
 
     public ResultVo(String code) {
-        this(code, "", null);
+        this(code, "");
     }
 
     public ResultVo(String code, String msg) {
-        this.code = code;
-        this.msg = msg;
+        this(code, msg, null);
     }
 
-    public ResultVo(String paramString1, String paramString2, Object paramObject) {
-        this.code = paramString1;
-        this.msg = paramString2;
-        this.data = paramObject;
+    public ResultVo(String code, String message, Object data) {
+        this.code = Optional.ofNullable(code).orElse("");
+        this.msg = Optional.ofNullable(message).orElse("");
+        this.data = data;
 
         //如果是列表的话 判断是否回传分页信息
-        if (paramObject instanceof List) {
+        if (data instanceof List) {
             HttpServletRequest httpServletRequest = Toolkits.getHttpServletRequest();
-            Boolean usePageHelper = Boolean.parseBoolean(Toolkits.defaultString(httpServletRequest.getAttribute("usePageHelper"),"false"));
+            boolean usePageHelper = Boolean.parseBoolean(Toolkits.defaultString(httpServletRequest.getAttribute("usePageHelper"), "false"));
 
-            if(usePageHelper) {
+            if (usePageHelper) {
                 Map<Object, Object> pageMap = Toolkits.getPageMap();
-                long total = new PageInfo((List) paramObject).getTotal();
+                long total = new PageInfo((List) data).getTotal();
 
                 long pageSize = Long.parseLong(Toolkits.defaultString(pageMap.get("pageSize"), "20"));
                 long totalPage = total % pageSize == 0 ? total / pageSize : (total / pageSize + 1);
@@ -56,12 +75,13 @@ public class ResultVo implements Serializable {
                 pageMap.put("total", total);
                 pageMap.put("totalPage", totalPage);
                 this.pageInfo = pageMap;
-                httpServletRequest.setAttribute("usePageHelper","false");
+                httpServletRequest.setAttribute("usePageHelper", "false");
             }
-        } else if(paramObject instanceof IPage){//如果传入的是MyBatisPlus 的分页对象
+        } else if (data instanceof IPage) {
+            //如果传入的是MyBatisPlus 的分页对象
             Map<Object, Object> pageMap = new HashMap<>();
-            
-            IPage page = (IPage) paramObject;
+
+            IPage page = (IPage) data;
             long total = page.getTotal();
             long pageSize = page.getSize();
             long pageNum = page.getCurrent();
@@ -76,69 +96,9 @@ public class ResultVo implements Serializable {
 
     }
 
-
     public static long getSerialVersionUID() {
         return serialVersionUID;
     }
 
-    public boolean isEncrypt() {
-        return encrypt;
-    }
-
-    public void setEncrypt(boolean encrypt) {
-        this.encrypt = encrypt;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-    public Object getPageInfo() {
-        return pageInfo;
-    }
-
-    public void setPageInfo(Object pageInfo) {
-        this.pageInfo = pageInfo;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getSignature() {
-        return signature;
-    }
-
-    public void setSignature(String signature) {
-        this.signature = signature;
-    }
-
-    public String getCreateTime() {
-        return this.createTime;
-    }
 
 }
